5a847e25bb3764b2c92a1bde06a829c07497dd23
Aina Rmngst <ainarmngst@gmail.com>
2025-04-08 10:54:35 +0300

    TD01: Ajouter un footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import mbdslogo from './assets/Logo.png'
 import './App.css'
 
 function Header(){
   return(
-    <div>
+    <header>
         <a href="https://univ-cotedazur.eu" target="_blank">
           <img src={mbdslogo} className="logo mbds" alt="ITU MBDS" />
         </a>
         <h1>Introduction à React</h1>
         <h3>A la découverte des premières notions de React</h3>
-      </div>
+      </header>
   )
 }
 function MainContent(){
   return(
-    <div>
+    <content>
       <h3>Ici nous afficherons des information interressante</h3>
-    </div>
+    </content>
   )
 }
-
+function Footer(){
+  return(
+    <footer>
+      <p>© 2025 - Heriaina.RAMANGASATA, Tous droits réservés.</p>
+    </footer>
+  )   
+}
 function App() {
   return (
     <>
+    <div className='app-container'>
       <Header />
 
       <MainContent />
+
+      <Footer />
+    </div>
+      
     </>
   )
 }
